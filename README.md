f69903e7756f4c5b68900dc09c58c80138ac935b
Eric Lee <eric.hyunil.lee@gmail.com>
2024-02-28 13:28:07 -0800

    Updated README and updated blog.js to work with null data from localStorage

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# my-first-blog
+# My First Blog
+
+The blog was created using HTML / CSS / Javascript files.
+
+## Features
+
+- Dark/Light theme modes are selectable by clicking on the sun/moon emoji at the top right corner.
+- The form data is saved in localStorage for it to be reloaded to the screen when the page is loaded.
+- User input validation in place to prevent empty strings to be entered.
+
+## Screenshots
+
+[!image of the working screen in deployed page](./assets/images/screenshot1.png)
+
+## Link to Deploy
+
+[Click here for deployed page](https://ericeya.github.io/my-first-blog)
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,26 +5,28 @@ const blogPosts = document.querySelector('.blog-posts')
 // This function loads what is to be shown to the user. For loop was used to iterate and depict all previous blog posts as well as the one that user just input.
 function loadFromLocalStorage() {
     const loadBlogData = JSON.parse(localStorage.getItem('blogData'))
-    for (let i=0; i<loadBlogData.length; i++) {
-        // Created variables for each of the object properties to be stored under a div element.
-        const blogList = document.createElement("div");
-        const blogTitle = document.createElement('h3');
-        const blogContent = document.createElement ('p');
-        const blogUser = document.createElement('p')
-        // Unique classes were given for each element created so that styling can be done thru CSS.
-        blogList.classList.add('blog-list');
-        blogTitle.classList.add('blog-title');
-        blogContent.classList.add('blog-content');
-        blogUser.classList.add('blog-user');
-        // Then each variables were appended to correct parent.
-        blogPosts.appendChild(blogList);
-        blogList.appendChild(blogTitle);
-        blogList.appendChild(blogContent);
-        blogList.appendChild(blogUser);
-        // Below pulls data from localStorage and generates strings onto the screen as blog posts.
-        blogTitle.textContent = `${loadBlogData[i].storedTitle}`;
-        blogContent.textContent = `${loadBlogData[i].storedContent}`;
-        blogUser.textContent = `Posted by: ${loadBlogData[i].storedUser}`;
+    if (loadBlogData!==null) {
+        for (let i=0; i<loadBlogData.length; i++) {
+            // Created variables for each of the object properties to be stored under a div element.
+            const blogList = document.createElement("div");
+            const blogTitle = document.createElement('h3');
+            const blogContent = document.createElement ('p');
+            const blogUser = document.createElement('p')
+            // Unique classes were given for each element created so that styling can be done thru CSS.
+            blogList.classList.add('blog-list');
+            blogTitle.classList.add('blog-title');
+            blogContent.classList.add('blog-content');
+            blogUser.classList.add('blog-user');
+            // Then each variables were appended to correct parent.
+            blogPosts.appendChild(blogList);
+            blogList.appendChild(blogTitle);
+            blogList.appendChild(blogContent);
+            blogList.appendChild(blogUser);
+            // Below pulls data from localStorage and generates strings onto the screen as blog posts.
+            blogTitle.textContent = `${loadBlogData[i].storedTitle}`;
+            blogContent.textContent = `${loadBlogData[i].storedContent}`;
+            blogUser.textContent = `Posted by: ${loadBlogData[i].storedUser}`;
+        }
     }
 }
 
